f12279fc674e7376cd0eb0a4fef6e70480bb8022
Kibiko <kevinjunchan@gmail.com>
2023-07-20 15:56:00 +0100

    parentheses checker complete with enhanced switches and stack

---
+++ src/main/java/ParenthesesChecker.java
@@ -0,0 +1,81 @@
+import java.util.Stack;
+
+public class ParenthesesChecker {
+
+    public boolean checkParentheses(String test){
+
+        //Test if empty
+        if (test.isEmpty()){
+            return true;
+        }
+
+        //Stacks brackets
+        Stack<Character> brackets = new Stack<>();
+
+        for (int i = 0; i < test.length(); i++) {
+            switch (test.charAt(i)) {
+                case '(', ')', '[', ']', '{', '}', '<', '>'
+                        -> brackets.push(test.charAt(i));
+            }
+        }
+
+        //Tests for odd amount of brackets
+        if (brackets.size() %2 !=0){
+            return false;
+        }
+
+        //Tests if same number of each bracket
+        int countOpenNorm = 0;
+        int countCloseNorm = 0;
+        int countOpenCorn = 0; 
+        int countCloseCorn = 0;
+        int countOpenCurl = 0;
+        int countCloseCurl = 0;
+        int countOpenSqr = 0;
+        int countCloseSqr = 0;
+
+
+        while (!brackets.isEmpty()){
+            switch (brackets.pop()) {
+                case '(' -> {
+                    if (countCloseNorm == 0) { //these test if there are wrong way brackets
+                        return false;        // e.g. ")(" or "(><)"
+                    }
+                    countOpenNorm += 1;
+                }
+                case ')' -> countCloseNorm += 1;
+                case '[' -> {
+                    if (countCloseSqr == 0) {
+                        return false;
+                    }
+                    countOpenSqr += 1;
+                }
+                case ']' -> countCloseSqr += 1;
+                case '{' -> {
+                    if (countCloseCurl == 0) {
+                        return false;
+                    }
+                    countOpenCurl += 1;
+                }
+                case '}' -> countCloseCurl += 1;
+                case '<' -> {
+                    if (countCloseCorn == 0) {
+                        return false;
+                    }
+                    countOpenCorn += 1;
+                }
+                case '>' -> countCloseCorn += 1;
+            }
+        }
+
+        if (countOpenNorm != countCloseNorm
+                || countOpenCorn != countCloseCorn
+                || countOpenCurl != countCloseCurl
+                || countOpenSqr != countCloseSqr){
+            return false;
+        }
+
+        //Otherwise true
+        return true;
+    }
+}
